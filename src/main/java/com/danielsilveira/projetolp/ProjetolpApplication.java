45909d178bed126b6efa76bc1bd0a192396a0a67
Daniel Silveira <drodrigues213@gmail.com>
2023-05-21 19:17:09 -0300

    Projeto LPII - Parte 1

---
+++ src/main/java/com/danielsilveira/projetolp/ProjetolpApplication.java
@@ -0,0 +1,12 @@
+package com.danielsilveira.projetolp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/danielsilveira/projetolp/ProjetolpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielsilveira.projetolp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetolpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
