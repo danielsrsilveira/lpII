fb195b73fe6bbb40d898aaf0b66480e08cda7199
Daniel Silveira <drodrigues213@gmail.com>
2023-05-21 20:42:57 -0300

    Parte 2

--- src/main/java/com/danielsilveira/projetolp/resources/CategoriaResource.java
+++ src/main/java/com/danielsilveira/projetolp/resources/CategoriaResource.java
@@ -4,8 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@RequestMapping("/categorias")
 @RestController
-@RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
